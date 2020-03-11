d21d867cf6aec90a6c3eb3fa9c511691972ff259
Robin Shaun <5675321+robin_shaun@user.noreply.gitee.com>
2020-03-11 21:13:44 +0800

    评测SLAM算法

---
+++ sensing/slam/vslam/evaluate/accuracy_evaluation.sh
@@ -0,0 +1,1 @@
+evo_ape bag 2020-03-03-22-05-17.bag /mavros/vision_pose/pose /orbslam2/vision_pose/pose -va --plot --plot_mode xy
--- sensing/slam/vslam/evaluate/record.sh
+++ sensing/slam/vslam/evaluate/record.sh
@@ -1,1 +1,1 @@
-rosbag record /mavros/vision_pose/pose /stereo/camera/left/camera_info /stereo/camera/left/image_raw /stereo/camera/right/camera_info /stereo/camera/right/image_raw 
+rosbag record /mavros/vision_pose/pose /stereo/camera/left/camera_info /stereo/camera/left/image_raw /stereo/camera/right/camera_info /stereo/camera/right/image_raw /orbslam2/vision_pose/pose
